605e79c6bb3c057942c3ec704bd77af2070678f8
ontorislamabdulla <65107640+ontorislamabdulla@users.noreply.github.com>
2020-05-10 11:09:23 +0300

    Update and rename zeroboy.py to ontorislamabdulla.py

--- ontorislamabdulla.py
+++ ontorislamabdulla.py
@@ -32,7 +32,7 @@ def acak(b):
 def cetak(b):
     w = 'ahtdzjc'
     for i in w:
-        j = w.index(i)
+        j = w.index(
         x= x.replace('!%s'%i,'\033[%s;1m'%str(31+j))
     x += '\033[0m'
     x = x.replace('!0','\033[0m')
@@ -49,17 +49,17 @@ def jalan(z):
 ##### LOGO #####
 logo = """ -----------------------------•◈•
 (  __)\\ ____--------------_------------•◈•
-|__(~)    •||•THE - AAHIL -OFFICAL------•◈•
-|__\~~) •||•RANA - RAJPUT---------------•◈•
-|__(-----\  •◈•------BLACK-TIGER--------•◈•
+|__(~)    •||•Ontor-Islam-Abdulla-OFFICAL------•💕😉•
+|__\~~) •||•Ontor-💜---------------•◈•
+|__(-----\  •◈•------System-maker•◈•
 |__~~~\ •◈•-----█-------⑦-------█------•◈•
 |__~~~\ •◈•-----█-------⑧-------█------•◈•
 |__~~~\ •◈•-----█-------⑥-------█------•◈•
 \033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;FlowRana
+\033[1;96mAuthor  \033[1;93m: \033[1;92/the.virtual.gamer
+\033[1;96mInstagram \033[1;93m: \033[1;Fllow Ontor_Islam_Abdulla
 \033[1;96mFacebook  \033[1;93m: \033[1; Aahilrna4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/Therana/zero
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://github.com/ontorislamabdulla
 \033[1;91m======================================="""
 
 def tik():
@@ -80,15 +80,15 @@ vuln = "\033[32mVuln"
 os.system("clear")
 print "\033[1;96m ============================================================="
 print  """\033[1;91m=======================================
-\033[1;96mAuthor  \033[1;93m: \033[1;92mRana Aahil
-\033[1;96mInstagram \033[1;93m: \033[1;92mFlowRana
-\033[1;96mFacebook  \033[1;93m: \033[1;92m Aahilrana4072
-\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/Therana/zero
+\033[1;96mAuthor  \033[1;93m: \033[1;92ontorvau
+\033[1;96mInstagram \033[1;93m: \033[1;92mFlow ontor
+\033[1;96mFacebook  \033[1;93m: \033[1;92m /The.Virtual.gamer
+\033[1;96mGithub \033[1;93m: \033[1;92mhttps://Github.com/clone/master
 \033[1;91m======================================="""
 print " \x1b[1;93m============================================================="
 
-CorrectUsername = "rana"
-CorrectPassword = "rana"
+CorrectUsername = "Ontorislamabdulla"
+CorrectPassword = "ontorvau"
 
 loop = 'true'
 while (loop == 'true'):
@@ -100,10 +100,10 @@ while (loop == 'true'):
             loop = 'false'
         else:
             print "Wrong Password"
-            os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+            os.system('xdg-open https://www.facebook.com/the.virtual.gamer
     else:
         print "Wrong Username"
-        os.system('xdg-open https://www.Youtube.com/UCsdJQbRf0xpvwaDu1rqgJuA')
+        os.system('xdg-open https://www.facebook.com/the.virtual.gamer
 
 def login():
 	os.system('clear')
